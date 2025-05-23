7aeab9bcfb0bfbbaf0c50f3d7dbb5b726607fd41
Margaret Santos de Oliveira <margaretsantos.adv@outlook.com>
2025-05-23 18:10:13 +0000

    Inicio dos navio - coordenadas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -11,5 +11,11 @@
 int main() {
     int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0};
 
+    int linha_navio_horizontal = 2; 
+    int coluna_navio_horizontal = 4; 
+
+    int linha_navio_vertical = 5; 
+    int coluna_navio_vertical = 1; 
+
     return 0;
 }
